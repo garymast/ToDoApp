90b8ce948ca32c237268adeb2f8f1c6756fbc9e7
garymast <>
2023-12-15 21:45:40 +0000

    Prepare further for deployment

--- flamenco/settings.py
+++ flamenco/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+
+import os
+
+if os.path.isfile("env.py"):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,11 +82,15 @@ WSGI_APPLICATION = 'flamenco.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://zaisaiti:anligoDY8U_sl4jThRBGQfJATZ3lizj8@flora.db.elephantsql.com/zaisaiti')
 }
 
 
